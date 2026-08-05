089001ef5fc613bb1160818eb14a8ee8356d4d4b
Gaurav Pareta <153729893+Gaurav-Pareta@users.noreply.github.com>
2026-08-05 17:17:29 +0530

    Time: 6 ms (42.98%), Space: 44 MB (98.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,17 @@
-import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] c1 = s.toCharArray();
-        char[] c2 = t.toCharArray();
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
         
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        
-        return Arrays.equals(c1,c2);
+        for(int i = 0; i<s.length() ; i++){
+            arr[s.charAt(i)-'a'] += 1;
+            arr[t.charAt(i)-'a'] -= 1;
+        }
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != 0) return false;
+        }
+        return true;
 
     }
 }
