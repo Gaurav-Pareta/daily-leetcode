f90ab148e20b65263d8165cd6cd4608c05f3b1cf
Gaurav Pareta <153729893+Gaurav-Pareta@users.noreply.github.com>
2026-09-08 23:55:25 +0530

    Time: 2 ms (99.95%), Space: 52.5 MB (65.04%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,19 +2,52 @@ import java.util.*;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length / 3;
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-        }
+        int el1 = 0, el2 = 0;
+        int count1 = 0, count2 = 0;
 
-        for(Map.Entry<Integer,Integer> e: map.entrySet()){
-            if(e.getValue() > n){
-                ans.add(e.getKey());
+        for (int num : nums) {
+
+            if (num == el1) {
+                count1++;
+            }
+            else if (num == el2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                el1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                el2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
+
+        // Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == el1) count1++;
+            else if (num == el2) count2++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+
+        if (count1 > n / 3) {
+            ans.add(el1);
+        }
+
+        if (count2 > n / 3) {
+            ans.add(el2);
+        }
+
         return ans;
     }
 }
