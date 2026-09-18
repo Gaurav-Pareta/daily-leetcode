8a65643bbb093696aa188baac030894734826348
Gaurav Pareta <153729893+Gaurav-Pareta@users.noreply.github.com>
2026-09-18 22:57:34 +0530

    Time: 4 ms (93.61%), Space: 70.6 MB (40.48%) - LeetHub

---
+++ 0565-array-nesting/0565-array-nesting.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int arrayNesting(int[] nums) {
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == -1) {
+                continue;
+            }
+
+            int current = i;
+            int count = 0;
+
+            while (nums[current] != -1) {
+                int next = nums[current];
+                nums[current] = -1;
+                current = next;
+                count++;
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
