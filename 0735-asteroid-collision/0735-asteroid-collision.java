13eda2c3339327f68a90b1b0839b9215d5501e20
Gaurav Pareta <153729893+Gaurav-Pareta@users.noreply.github.com>
2026-09-26 13:35:28 +0530

    Time: 5 ms (63.99%), Space: 47.1 MB (27.69%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < asteroids.length; i++) {
+            if (asteroids[i] > 0) {
+                s.push(asteroids[i]);
+            } else {
+                while (!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(asteroids[i])) {
+                    s.pop();
+                }
+
+                if (!s.isEmpty() && s.peek() == Math.abs(asteroids[i])) {
+                    s.pop();
+                } else if (s.isEmpty() || s.peek() < 0) {
+                    s.push(asteroids[i]);
+                }
+            }
+
+        }
+        int[] res = new int[s.size()];
+        for (int i = s.size() - 1; i >= 0; i--) {
+            res[i] = s.pop();
+        }
+
+        return res;
+    }
+}
